53d309cc9e5aa53c9a057ba2923b30694b8f1a5d
marikvntu <marikvntu@gmail.com>
2013-09-28 12:52:57 +0300

    WCF service introduction.
    NOT-WORKING, for sync purpose only.

---
+++ backup/IJunkService.cs
@@ -0,0 +1,20 @@
+﻿using JunkBackEnd.Entities;
+using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace VinnitsaJunkFood
+{
+    
+    [ServiceContract]
+    public interface IJunkService
+    {
+        [OperationContract]
+
+        [WebInvoke (Method = "GET",
+                    ResponseFormat = WebMessageFormat.Json,
+                    BodyStyle = WebMessageBodyStyle.Bare,
+                    UriTemplate = "DoWork")]
+        string DoWork();
+    }
+}
---
+++ backup/JunkService.svc.cs
@@ -0,0 +1,27 @@
+﻿using JunkBackEnd.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace VinnitsaJunkFood
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "JunkService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select JunkService.svc or JunkService.svc.cs at the Solution Explorer and start debugging.
+
+    
+    public class JunkService : IJunkService
+    {
+        
+        public string DoWork()
+        {
+            //return new CommentEntity() {
+            //     CommentText = "idi v pizdu"
+            //};
+
+            return "ololo";
+        }
+    }
+}
